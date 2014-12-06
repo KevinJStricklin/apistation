d648b4df879005861ae419e657b347e3cb43e1bd
rexwhitten <rex.whitten@gmail.com>
2014-12-06 11:44:10 -0500

    changing the JSON POST behavior

---
+++ nodestation/middleware/index.js
@@ -0,0 +1,13 @@
+﻿
+// Middleware Namespace
+
+var Middleware = {}; // Attach discrete middleware objects to this namespace 
+
+Middleware.CORS_Middleware = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+exports.Middleware = Middleware;
